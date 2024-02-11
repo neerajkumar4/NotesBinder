a552edbc4161974513ad24cc5382b6dd62031097
Neeraj Kumar <nk4976744@gmail.com>
2024-02-11 16:55:31 +0530

    auth api added

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 mongoose
@@ -17,7 +18,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/auth", authRouter);
 
 app.listen(4000, () => {
   console.log("Server is running at port 4000!");
+});
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    return res
+      .status(err.statusCode)
+      .json({ success: false, message: err.message });
+  }
+  res.status(500).json({ success: false, message: err.message });
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Signin, Signup } from "./pages";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
